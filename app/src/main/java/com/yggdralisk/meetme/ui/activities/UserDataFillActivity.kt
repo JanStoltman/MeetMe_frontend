db0d007438fb8a1ba8d52377182e2b731633ed07
szulcmaciej <szulcmaciej1@gmail.com>
2018-05-06 12:22:36 +0200

    UserDataFillActivity added

---
+++ app/src/main/java/com/yggdralisk/meetme/ui/activities/UserDataFillActivity.kt
@@ -0,0 +1,15 @@
+package com.yggdralisk.meetme.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.yggdralisk.meetme.R
+
+class UserDataFillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_data_fill)
+    }
+
+
+}
